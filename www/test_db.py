ef637fea57c6a742a05eaa1fb4b834b78ea614e0
liunnis <liunnis@qq.com>
2015-08-01 14:03:48 +0800

    myself
    201508011403

--- www/test_db.py
+++ www/test_db.py
@@ -3,13 +3,13 @@
 from models import User, Blog, Comment
 from transwarp import db
 db.create_engine(user='www-data', password='www-data', database='awesome')
-u = User(name='Test', email='test@example.com',
+u = User(name='Test0951', email='test0951@example.com',
          password='1234567890', image='about:blank')
 u.insert()
-print 'new user id:', u.id
-u1 = User.find_first('where email = ?', "test@example.com")
-print 'find user \'s name:', u1.name
-u1.delete()
+# print 'new user id:', u.id
+# u1 = User.find_first('where email = ?', "test@example.com")
+# print 'find user \'s name:', u1.name
+# u1.delete()
 
-u2 = User.find_first('where email=?', 'test@example.com')
-print 'find user:', u2
+# u2 = User.find_first('where email=?', 'test@example.com')
+# print 'find user:', u2
