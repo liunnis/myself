89c3235475ec04b001002f535f2ef4b3db241c95
liunnis <liunnis@qq.com>
2015-08-02 22:55:33 +0800

    myself
    201508022255

--- www/test_db.py
+++ www/test_db.py
@@ -6,6 +6,7 @@ db.create_engine(user='www-data', password='www-data', database='awesome')
 u = User(name='Test0951', email='test0951@example.com',
          password='1234567890', image='about:blank')
 u.insert()
+# u.
 # print 'new user id:', u.id
 # u1 = User.find_first('where email = ?', "test@example.com")
 # print 'find user \'s name:', u1.name
